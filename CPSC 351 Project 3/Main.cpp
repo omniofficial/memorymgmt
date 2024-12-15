03554673d559408323cf54c5cf6f548afd2d1c1b
Eddy <fdlguerra0@gmail.com>
2024-12-15 15:36:44 -0800

    Main function/driver program

--- CPSC 351 Project 3/Main.cpp
+++ CPSC 351 Project 3/Main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <string>
+
+int inputMemSize = 0;
+int inputPageSize = 0;
+int pageSize = 0;
+std::string inputFile = "";
+std::string outputFile = "";
+
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello world!" << endl;
+	cout << "Enter the memory size:  " << endl;
+	cin >> inputMemSize;
+
+	cout << "Enter the page size: " << endl;
+	cin >> inputPageSize;
+
+
+	if (inputPageSize == 1)
+	{
+		pageSize = 100;
+	}
+	else if (inputPageSize == 2)
+	{
+		pageSize = 200;
+	}
+	else if (inputPageSize == 3)
+	{
+		pageSize = 300;
+	}
+	else
+	{
+		cout << "Error. Invalid input." << endl;
+	}
+
+
+	system("pause");
+	return 0;
 }
